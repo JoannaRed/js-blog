e5c781783299515c14b356964f477e36dc374ea9
Joanna Baranowska <j.baranowska@hotmail.com>
2020-07-16 19:29:04 +0200

    Add delate .active

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const titleClickHandler = function(event){
   console.log('Link was clicked!')
-  console.log('clic', event );
+  /*console.log('clic', event );*/
 
   /* remove class 'active' from all article links  */
+  
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* add class 'active' to the clicked link */
 
