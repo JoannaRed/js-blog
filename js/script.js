147d52b71c6ede75a816697236345c7854885ddb
Joanna Baranowska <j.baranowska@hotmail.com>
2020-07-17 13:48:53 +0200

    add active articles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const titleClickHandler = function(event){
   console.log('Link was clicked!')
   /*console.log('clic', event );*/
 
-  /* remove class 'active' from all article links  */
+  /*[DONE] remove class 'active' from all article links  */
   
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -10,18 +10,22 @@ const titleClickHandler = function(event){
   activeLink.classList.remove('active');
 }
 
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-const activeArticles = document.querySelectorAll('.titles a');
+  const clickedElement = this;
+  console.log('clickedElement:', clickedElement);
+  console.log('clickedElement (with plus): ' + clickedElement);
+
+  const clickedElement = function(){
+  activeLink.classList.add('active')};
+
+  /* [DONE] remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.titles a');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
-
-  /* add class 'active' to the clicked link */
-
-  
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
