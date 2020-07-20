0f75f695c5dab9d3b7b18dc4855d11670d0da962
Joanna Baranowska <j.baranowska@hotmail.com>
2020-07-20 17:58:28 +0200

    Add tagretArticle

--- js/script.js
+++ js/script.js
@@ -31,13 +31,15 @@ const titleClickHandler = function(event){
       activeArticle.classList.remove('active');
   }
   
-/* get 'href' attribute from the clicked link */
+/* [IN PROGRESS ] get 'href' attribute from the clicked link */
 
-  const articleSelector = clickedElement.getAttribute('href')
-  console.log(articleSelector)
+    const articleSelector = clickedElement.getAttribute('href')
+  //console.log(articleSelector)
   
-/* find the correct article using the selector (value of 'href' attribute) */
+/* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
   
+    const targetArticle = document.querySelector('href', articleSelector) 
+    console.log(targetArticle)
 /* add class 'active' to the correct article */
   }
   
