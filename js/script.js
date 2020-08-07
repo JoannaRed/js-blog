fd26ab74f6fef358d79329558c36423aeb778be9
Joanna Baranowska <j.baranowska@hotmail.com>
2020-08-07 19:00:11 +0200

    Add find-title

--- js/script.js
+++ js/script.js
@@ -50,14 +50,21 @@ function generateTitleLinks(){
 
   document.querySelectorAll(optTitleListSelector).innerHTML = ''
 
-  /* for each article */
+  /* (done) for each article */
 
-  const articles =  document.querySelectorAll('.post');  
+  const articles =  document.querySelectorAll('.post');
+  
+  for(let article of articles){
+    link.addEventListener('click', titleClickHandler );
 
-    /* get the article id */
+  /* get the article id */
+
+    const articleId = clickedElement.getAttribute('id');
 
     /* find the title element */
 
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    
     /* get the title from the title element */
 
     /* create HTML of the link */
