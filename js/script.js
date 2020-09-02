80405ecde156c3b2f207d0cf7c30cd78698b3686
Joanna Baranowska <j.baranowska@hotmail.com>
2020-09-02 13:47:49 +0200

    Modifed generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -146,32 +146,47 @@ function generateTags() {
 generateTags();
 
 function tagClickHandler(event){
-  //console.log(this);
-  /* prevent default action for this event */
   
+  /* prevent default action for this event */
+  console.log(tagClickHandler) 
+
   /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = this
 
   /* make a new constant "href" and read the attribute "href" of the clicked element */
-
+  const href = document.clickedElement.getAttribute('href')
+  
   /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
 
   /* find all tag links with class active */
 
+  let activeTag = document.querySelectorAll('a.active[href^="#tag-"]')
+
   /* START LOOP: for each active tag link */
+  for(let tag of activeTag){
 
     /* remove class active */
+    activeTag.classList.remove('active'); 
 
   /* END LOOP: for each active tag link */
+  }
 
   /* find all tag links with "href" attribute equal to the "href" constant */
+  let tagLinks = document.querySelectorAll('a[href="' + href + '"]'')
 
   /* START LOOP: for each found tag link */
+  for(let tag of tagLinks){
 
     /* add class active */
+    tag.classList.add('active');
 
   /* END LOOP: for each found tag link */
-
+  } 
+   
   /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks('[data-tags~="' + tag + '"]');
+
 }
 
 function addClickListenersToTags(){
